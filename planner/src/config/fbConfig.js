8124dc76e2b0d1ed3a5ef9d7bc964b68cb4e1e46
LadyKerr <kadiekerr@gmail.com>
2019-08-16 00:39:02 -0400

    initialize firebaseeee!

---
+++ planner/src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDgz5ie3PpGNkSQE-Z37kZmurRzAkAVF2I",
+  authDomain: "taylor-planner.firebaseapp.com",
+  databaseURL: "https://taylor-planner.firebaseio.com",
+  projectId: "taylor-planner",
+  storageBucket: "",
+  messagingSenderId: "760173495179",
+  appId: "1:760173495179:web:9e00e2953053f1e9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
