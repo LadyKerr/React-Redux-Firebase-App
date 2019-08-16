8cedd1e7115698d323641ce6db9d613f97d7c325
LadyKerr <kadiekerr@gmail.com>
2019-08-16 11:57:52 -0400

    error resolved. downgraded react-redux and react-redux-firebase versions

--- planner/src/components/dashboard/Dashboard.js
+++ planner/src/components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React from "react";
 import ProjectList from "../projects/ProjectList";
 import Notifications from "./Notifications";
 import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 class Dashboard extends React.Component {
   render() {
@@ -24,8 +26,14 @@ class Dashboard extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
-  projects: state.projectReducer.projects
-});
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    projects: state.projectReducer.projects
+  };
+};
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "projects" }]) //when the component is loaded, we want to find the projects collection in db; actively listening
+)(Dashboard);
--- planner/src/reducers/rootReducer.js
+++ planner/src/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import { projectReducer } from "./projectReducer";
 import { authReducer } from "./authReducer";
+import { firestoreReducer } from "redux-firestore";
+// import { firebaseReducer } from "react-redux-firebase";
 
 export default combineReducers({
   projectReducer,
-  authReducer
+  authReducer,
+  firestoreReducer,
+  // firebaseReducer
 });
