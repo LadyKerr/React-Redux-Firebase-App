7908f01cb52bcc9e298a07bcab5f0d5ebdcf016b
LadyKerr <kadiekerr@gmail.com>
2019-08-16 16:15:17 -0400

    authReducer and authActions created

--- planner/src/reducers/authReducer.js
+++ planner/src/reducers/authReducer.js
@@ -1,5 +1,22 @@
-const initialState = {};
+const initialState = {
+  authError: null
+};
 
 export const authReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case "LOGIN_ERROR":
+      console.log("login error", err);
+      return {
+        ...state,
+        authError: "Login Failed"
+      };
+    case "LOGIN_SUCCESS":
+      console.log("login success");
+      return {
+        ...state,
+        authError: null
+      };
+    default:
+      return state;
+  }
 };
--- planner/src/reducers/rootReducer.js
+++ planner/src/reducers/rootReducer.js
@@ -2,11 +2,11 @@ import { combineReducers } from "redux";
 import { projectReducer } from "./projectReducer";
 import { authReducer } from "./authReducer";
 import { firestoreReducer } from "redux-firestore";
-// import { firebaseReducer } from "react-redux-firebase";
+import { firebaseReducer } from "react-redux-firebase";
 
 export default combineReducers({
   projectReducer,
   authReducer,
   firestoreReducer,
-  // firebaseReducer
+  firebaseReducer //does the auth syncing
 });
