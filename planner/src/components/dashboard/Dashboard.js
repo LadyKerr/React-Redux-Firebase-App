594bde8afacf7a6d281af2b11a3d8b50c6ba008e
LadyKerr <kadiekerr@gmail.com>
2019-08-17 21:21:27 -0400

    protected routes

--- planner/src/components/dashboard/Dashboard.js
+++ planner/src/components/dashboard/Dashboard.js
@@ -4,32 +4,38 @@ import Notifications from "./Notifications";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
+import { Redirect } from "react-router-dom";
 
 class Dashboard extends React.Component {
   render() {
-    // console.log(this.props);
-    const projects = this.props.projects;
-    console.log(projects);
+    // destructor projects & auth from props
+    const { projects, auth } = this.props;
 
-    return (
-      <div className="dashboard container">
-        <div className="row">
-          <div className="col s12 m6">
-            <ProjectList projects={projects} />
-          </div>
-          <div className="col s12 m5 offset-m1">
-            <Notifications />
+    //so if the user isnt logged in, they will be redirected to the signin page!
+    if (!auth.uid) {
+      return <Redirect to="/signin" />;
+    } else {
+      return (
+        <div className="dashboard container">
+          <div className="row">
+            <div className="col s12 m6">
+              <ProjectList projects={projects} />
+            </div>
+            <div className="col s12 m5 offset-m1">
+              <Notifications />
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
 const mapStateToProps = state => {
   // console.log(state);
   return {
-    projects: state.firestoreReducer.ordered.projects
+    projects: state.firestoreReducer.ordered.projects,
+    auth: state.firebaseReducer.auth
   };
 };
 
