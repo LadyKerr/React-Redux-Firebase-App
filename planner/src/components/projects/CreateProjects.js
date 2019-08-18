52cff7f7aaceaa6ebe665836989ff913a23a3a99
LadyKerr <kadiekerr@gmail.com>
2019-08-17 21:29:30 -0400

    proted project details and create new project routes

--- planner/src/components/projects/CreateProjects.js
+++ planner/src/components/projects/CreateProjects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createProject } from "../../actions/projectActions";
+import { Redirect } from "react-router-dom";
 
 class CreateProject extends React.Component {
   state = {
@@ -21,6 +22,12 @@ class CreateProject extends React.Component {
   };
 
   render() {
+    const { auth } = this.props;
+
+    if (!auth.uid) {
+      return <Redirect to="/sign" />;
+    }
+
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -52,7 +59,14 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
+const mapStateToProps = state => {
+  // console.log(state);
+  return {
+    auth: state.firebaseReducer.auth
+  };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CreateProject);
