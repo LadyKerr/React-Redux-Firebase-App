f77eab3660ce712f6c7788c95977a1c96b1cd9e2
LadyKerr <kadiekerr@gmail.com>
2019-08-19 08:18:37 -0400

    notification component & first cloudFunction

--- planner/functions/index.js
+++ planner/functions/index.js
@@ -1,8 +1,13 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+//we are creating a function called hello world then attaching it to the
+//https on functions and then we telling it on Request we want to send the message
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Planner React-App!");
+});
+
+//once deployed the function will run on request via the specific url (its like an endpoint for the function)
